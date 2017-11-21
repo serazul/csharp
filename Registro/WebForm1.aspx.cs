fdb3aefe160ecceea73e582742123017ef291a32
sergio <chechomindtricks@gmail.com>
2017-11-21 08:46:05 -0500

    ok

--- Registro/WebForm1.aspx.cs
+++ Registro/WebForm1.aspx.cs
@@ -10,6 +10,11 @@ namespace Registro
     using System.Web.UI.WebControls;
     using Registro.Models;
     using System.Web.ModelBinding;
+    using System.IO;
+    using System.IO.Pipes;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
 
     public partial class WebForm1 : System.Web.UI.Page
     {
@@ -22,9 +27,17 @@ namespace Registro
             UserData dataObject = new UserData();
             if (TryUpdateModel(dataObject, new FormValueProvider(ModelBindingExecutionContext)))
             {
-                DataText.InnerText = String.Format("Nombre: {0}, Apellido: {1}, Email: {2}, Zip: {3}, Genero: {4}, Pais: {5}",
+              var data =  DataText.InnerText = String.Format("Nombre: {0}, Apellido: {1}, Email: {2}, Zip: {3}, Genero: {4}, Pais: {5}",
                     dataObject.Nombre, dataObject.Apellido, dataObject.Email, dataObject.Zip, dataObject.Genero, dataObject.Pais);
-                   
+
+
+                
+
+                using (StreamWriter file = File.CreateText(@"c:\data.json"))
+                    {
+                       JsonSerializer serializer = new JsonSerializer();
+                        serializer.Serialize(file, data);
+                    }
             }
         }
         protected void btnSubmit_Click(object sender, EventArgs e)
@@ -37,5 +50,6 @@ namespace Registro
             ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('" + msg + "');</script>");
         }
 
-    }
+     
+}
 }
